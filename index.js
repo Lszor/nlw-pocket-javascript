2999f688d400360fca6fe9869da8787c0d0a6f95
Lszor <126995724+Lszor@users.noreply.github.com>
2024-09-10 18:02:59 -0300

    adiciona listarMetas

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //importa caixa de seleção e inputs dos prompts
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
   value: "teste",
   checked: false
 }
 
-let metas = [ meta ]
+let metas = [meta]
 
 const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite sua meta:" })
@@ -20,15 +20,31 @@ const cadastrarMeta = async () => {
   })
 }
 
-// const listarMetas = () => {
-//   if (metas.length == 0) {
-//     console.log("não há nenhuma meta.");
-//   }
+const listarMetas = async () => {
+  const respostas = await checkbox(
+    {
+      message: "use ↑ e ↓ para navegar entre as metas, tecla espaço para marcar e desmarcar e Enter para finalizar esta etapa.",
+      choices: [...metas],
+      instructions: false
+    }
+  )
+  if (respostas.length == 0) {
+    console.log("nenhuma meta selecionada");
+    return
+  }
 
-//   for (let pos in metas) {
-//     console.log(metas)
-//   }
-// }
+  metas.forEach((m) => {
+    m.checked = false
+  })
+  
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta
+    })
+    meta.checked = true
+  })
+  console.log("meta(s) marcada(s) como concluída(s)");
+}
 
 //arrow function
 const start = async () => {
@@ -59,7 +75,7 @@ const start = async () => {
         // console.log("cadastrado");
         break
       case "listar":
-        listarMetas()
+        await listarMetas()
         // console.log(metas);
         break
       case "sair":
