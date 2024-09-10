ada04cb764b5c2c1ff1499eb1af97241c50cf88e
Lszor <126995724+Lszor@users.noreply.github.com>
2024-09-10 15:29:15 -0300

    adiciona index.js e menu base

---
+++ index.js
@@ -0,0 +1,16 @@
+//arrow function
+const start = () => {
+  
+  while(true){
+    switch(opcao){
+      case "cadastrar":
+        console.log("cadastrado");
+      break;
+      case "listar":
+      console.log("listados");
+      break;
+      case "sair":
+        return
+    }
+  }
+}
