b592d3cc01d95989257eb260c85a59f66c376371
Lszor <126995724+Lszor@users.noreply.github.com>
2024-09-10 16:05:13 -0300

    inicia menu do app

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+//importa caixa de seleção dos prompts
+const { select } = require('@inquirer/prompts')
+
 //arrow function
-const start = () => {
-  
-  while(true){
-    switch(opcao){
+const start = async () => {
+
+  while (true) {
+
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar Meta",
+          value: "cadastrar"
+        },
+        {
+          name: "listar Metas",
+          value: "listar"
+        },
+        {
+          name: "Sair",
+          value: "sair"
+        }
+      ]
+    })
+    
+    switch (opcao) {
       case "cadastrar":
         console.log("cadastrado");
-      break;
+        break
       case "listar":
-      console.log("listados");
-      break;
+        console.log("listados");
+        break
       case "sair":
         return
     }
   }
 }
+
+start()
